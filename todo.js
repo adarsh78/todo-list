31a2650f69e678b03040d7f292b5a26b7954a925
adarsh78 <adarshgupta7ag@gmail.com>
2023-04-13 12:06:02 +0530

    second commit

--- todo.js
+++ todo.js
@@ -4,28 +4,38 @@ const todoListElement = document.getElementById('todo-list');
 
 addBtnElement.addEventListener('click', addATodoItem);
 
-const todoList = [];
+let todoList = [];
 
 function addATodoItem(event) {
   event.preventDefault();
 
   const todoText = todoInputElement.value;
+  if (!todoText.length) return;
+
   todoList.push(todoText);
   todoInputElement.value = '';
-
 
   renderTodoList();
 }
 
-
 function renderTodoList() {
-  const listToRender = todoList.map(listItem => `
+  const listToRender = todoList.map((listItem, index) => `
       <div class="list-item">
         <div>${listItem}</div>
-        <button>delete</button>
+        <button onclick="performDelete(${index})">delete</button>
       </div>
     `);
   todoListElement.innerHTML = listToRender.join('');
+}
+
+function performDelete(index) {
+  const confirmDelete = window.confirm(`TODO Item: ${todoList[index]} \nAre you sure you want to delete this todo item?`);
+
+  if (confirmDelete) {
+    todoList = todoList.filter((item, i) => i !== index);
+    renderTodoList();
+  }
+
 }
 
 renderTodoList();
